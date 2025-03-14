8706a34c3aa1c7e61404a83f99f2adc78a52b7a1
Malla Sudharshan Sai Sunny <sudarshan.malla75@gmail.com>
2025-03-14 11:12:59 +0530

    Add files via upload

---
+++ Simple_Calculator/src/main/java/com/mycompany/simple_calculator/Simple_Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simple_calculator;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Simple_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
